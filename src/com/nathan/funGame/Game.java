2cf95d50086ef881c216a00d54811de7f6278b45
AaronStaar <exdracr256@gmail.com>
2015-06-25 10:08:25 -0700

    made loops for an arraylist.

--- src/com/nathan/funGame/Game.java
+++ src/com/nathan/funGame/Game.java
@@ -1,21 +1,30 @@
 package com.nathan.funGame;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
+import com.nathan.funGame.entities.BaseEntity;
+
 
 public class Game extends BasicGame {
-
+	
+	private ArrayList<BaseEntity> entityList;
+	
 	public Game(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
+		this.entityList = new ArrayList<BaseEntity>();
 	}
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < entityList.size(); i++) {
+			entityList.get(i).render(container, g);
+		}
 		
 	}
 
@@ -27,8 +36,8 @@ public class Game extends BasicGame {
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		for (BaseEntity e : entityList) {
+			e.update(container, delta);
+		}
 	}
-
 }
